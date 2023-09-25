b3ea7d24b9948b2493db165cd96a1bfcc42711ae
Tadg Keating Webb <tadgkeating@gmail.com>
2023-09-25 17:26:31 +0700

    Added links in top and bottom menu for pages that exist

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -26,10 +26,12 @@ import { Link } from "@inertiajs/vue3";
                 <div class="collapse navbar-collapse w-100 pt-3 pb-2 py-lg-0" id="navigation">
                     <ul class="navbar-nav navbar-nav-hover ms-auto">
                         <li class="nav-item mx-2">
-                            <a href="javascript:void(0);" class="nav-link ps-2 cursor-pointer">Trips</a>
+                            <Link :href="route('trip.index')" :class='"nav-link ps-2 cursor-pointer"'>
+                                Trips
+                            </Link>
                         </li>
                         <li class="nav-item mx-2">
-                            <a href="javascript:void(0);" class="nav-link ps-2 cursor-pointer">Review</a>
+                            <a href="javascript:void(0);" class="nav-link ps-2 cursor-pointer">Reviews</a>
                         </li>
                         <li class="nav-item mx-2">
                             <Link :href="route('contact.create')" :class='"nav-link ps-2 cursor-pointer"'>
@@ -49,56 +51,56 @@ import { Link } from "@inertiajs/vue3";
         </main>
 
         <footer class="footer py-5 bg-gradient-dark mt-auto py-3 mb-0">
-            `            <div class="container position-relative z-index-1">
-            <div class="row">
-                <div class="col-lg-4 me-auto mb-lg-0 mb-4 text-lg-start text-center">
-                    <h6 class="text-white font-weight-bolder text-uppercase mb-lg-4 mb-3">Hotel Management System</h6>
-                    <ul class="nav flex-row ms-n3 align-items-center mb-4 mt-sm-0">
-                        <li class="nav-item">
-                            <a href="javascript:void(0);"  class="nav-link text-white opacity-8">
-                                Home
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white opacity-8" href="javascript:;">
-                                Trips
-                            </a>
-                        </li>
+            <div class="container position-relative z-index-1">
+                <div class="row">
+                    <div class="col-lg-4 me-auto mb-lg-0 mb-4 text-lg-start text-center">
+                        <h6 class="text-white font-weight-bolder text-uppercase mb-lg-4 mb-3">Hotel Management System</h6>
+                        <ul class="nav flex-row ms-n3 align-items-center mb-4 mt-sm-0">
+                            <li class="nav-item">
+                                <Link :href="route('home')" :class='"nav-link text-white opacity-8"'>
+                                    Home
+                                </Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link :href="route('trip.index')" :class='"nav-link text-white opacity-8"'>
+                                    Trips
+                                </Link>
+                            </li>
 
-                        <li class="nav-item">
-                            <a class="nav-link text-white opacity-8" href="javascript:;">
-                                Booking
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="javascript:void(0);" class="nav-link text-white opacity-8">
-                                Contact
-                            </a>
-                        </li>
-                    </ul>
-                    <p class="text-sm text-white opacity-8 mb-0">
-                        Copyright &#174; 2023 Tadg Keating-Webb&#8482;
-                    </p>
-                </div>
-                <div class="col-lg-6 ms-auto text-lg-end text-center">
-                    <p class="mb-5 text-lg text-white font-weight-bold">
-                        Unwind, Connect, and Make Lasting Memories at Our Vibrant Hotel!
-                    </p>
-                    <a href="javascript:;" class="text-white me-xl-4 me-4 opacity-5">
-                        <span class="fab fa-instagram" aria-hidden="true"></span>
-                    </a>
-                    <a href="javascript:;" class="text-white me-xl-4 me-4 opacity-5">
-                        <span class="fab fa-twitter" aria-hidden="true"></span>
-                    </a>
-                    <a href="javascript:;" class="text-white me-xl-4 me-4 opacity-5" target="_blank">
-                        <span class="fab fa-facebook" aria-hidden="true"></span>
-                    </a>
-                    <a href="javascript:;" class="text-white me-xl-4 me-4 opacity-5">
-                        <span class="fab fa-youtube" aria-hidden="true"></span>
-                    </a>
+                            <li class="nav-item">
+                                <a class="nav-link text-white opacity-8" href="javascript:;">
+                                    Booking
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <Link :href="route('contact.create')" :class='"nav-link text-white opacity-8"'>
+                                    Contact
+                                </Link>
+                            </li>
+                        </ul>
+                        <p class="text-sm text-white opacity-8 mb-0">
+                            Copyright &#174; 2023 Tadg Keating-Webb&#8482;
+                        </p>
+                    </div>
+                    <div class="col-lg-6 ms-auto text-lg-end text-center">
+                        <p class="mb-5 text-lg text-white font-weight-bold">
+                            Unwind, Connect, and Make Lasting Memories at Our Vibrant Hotel!
+                        </p>
+                        <a href="javascript:;" class="text-white me-xl-4 me-4 opacity-5">
+                            <span class="fab fa-instagram" aria-hidden="true"></span>
+                        </a>
+                        <a href="javascript:;" class="text-white me-xl-4 me-4 opacity-5">
+                            <span class="fab fa-twitter" aria-hidden="true"></span>
+                        </a>
+                        <a href="javascript:;" class="text-white me-xl-4 me-4 opacity-5" target="_blank">
+                            <span class="fab fa-facebook" aria-hidden="true"></span>
+                        </a>
+                        <a href="javascript:;" class="text-white me-xl-4 me-4 opacity-5">
+                            <span class="fab fa-youtube" aria-hidden="true"></span>
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
         </footer>
     </div>
 </template>
